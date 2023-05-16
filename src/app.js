3a62d561357ebf95076c83d12c786c22e4b13bd1
cowen27 <calvin.owen27@gmail.com>
2023-05-15 21:28:32 -0700

    working on post requests

--- src/app.js
+++ src/app.js
@@ -1,38 +1,37 @@
 import express from 'express';
-
 import { config } from 'dotenv';
 import { executeCrudOperations } from './main.js';
-
-import * as bodyParse from 'body-parser';
-
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-
-//import { config } from 'dotenv';
-//import { executeCrudOperations } from './main.js';
+import bodyParser from 'body-parser';
 
 config();
-//executeCrudOperations();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
 
+const PORT = 3000;
+
 app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(3000, function (req, res) {
-  console.log("Server is running on localhost3000");
-
-  //config();
-  //executeCrudOperations();
+app.listen(PORT, function (req, res) {
+  console.log("Server is running on port " + PORT);
 });
 
 app.post('/clicked', (req, res) => {
-  executeCrudOperations();
-  res.sendStatus(201);
+  var text = req.body.testinput;
+  console.log(req.body);
+  //console.log(req.headers.host);
+  //executeCrudOperations();
+  res.writeHead(301, {Location: req.headers.host});
+  res.end();
+  res.send();
 });
